7649909b503bafd601913b81412b25f39d8a033a
robrtruns <robrtjaxon@gmail.com>
2020-04-23 08:05:34 -0500

    Adding css

---
+++ employee-directory/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/Component-style.css';
+
+function Header() {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+      <h1 class="display-4">Employee Directory</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
